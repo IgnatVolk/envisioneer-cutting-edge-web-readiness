482471185cbe1df21a8e661d902fed5f71e3a824
IgnatVolk <142455386+IgnatVolk@users.noreply.github.com>
2023-12-15 20:36:24 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,30 @@
+function generateRandomData() {
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+    return data;
+    });
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+}
+
+
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+    }
+    const data = [];
+}
+function main() {    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+    return data
+
+
+import random
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+def main():
+    main()
+    for item in data:
+if __name__ == "__main__":
